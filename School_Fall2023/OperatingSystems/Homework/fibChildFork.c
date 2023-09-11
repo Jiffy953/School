076c0aae38a809d63ca9f2e200e791bb4057a51a
Jared See <jiffy@Jareds-MacBook-Pro.local>
2023-09-11 11:22:08 -0400

    pre-c99 syntax

--- School_Fall2023/OperatingSystems/Homework/fibChildFork.c
+++ School_Fall2023/OperatingSystems/Homework/fibChildFork.c
@@ -4,11 +4,20 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+#define MAX_CHILDREN 3
+
 int fibonacci(int n);
 
 int main(int argc, char *argv[])
 {
-    if(argc-1 > 3)
+    int i;
+    int results[MAX_CHILDREN];
+    int fd[2];
+    pid_t child_pid;
+    int total_sum;
+    int n, fib_result;
+
+    if(argc-1 > MAX_CHILDREN)
     {
         printf("Sorry, only three children are allowed at most.\n");
         return EXIT_FAILURE;
@@ -20,7 +29,7 @@ int main(int argc, char *argv[])
     }
 
     printf("I am parent with pid: %d sending", getpid());
-    for(int i = 1; i < argc; i++)
+    for(i = 1; i < argc; i++)
     {
         printf(" %s to one of my children", argv[i]);
         if(i < argc - 1)
@@ -28,22 +37,20 @@ int main(int argc, char *argv[])
     }
     printf(".\n");
     
-    int results[argc - 1];
-    for(int i = 1; i < argc; i++)
+    for(i = 1; i < argc; i++)
     {
-        int fd[2];
         if (pipe(fd) == -1)
         {
             perror("pipe");
             return EXIT_FAILURE;
         }
 
-        pid_t child_pid = fork();
+        child_pid = fork();
         if(child_pid == 0)
         {
             close(fd[0]);
-            int n = atoi(argv[i]);
-            int fib_result = fibonacci(n);
+            n = atoi(argv[i]);
+            fib_result = fibonacci(n);
             printf("I am child with pid: %d, sending %d to my parent.\n", getpid(), fib_result);
             write(fd[1], &fib_result, sizeof(fib_result));
             close(fd[1]);
@@ -57,9 +64,9 @@ int main(int argc, char *argv[])
         }
     }
 
-    int total_sum = 0;
+    total_sum = 0;
     printf("I am the parent pid: %d receiving", getpid());
-    for(int i = 0; i < argc - 1; i++)
+    for(i = 0; i < argc - 1; i++)
     {
         total_sum += results[i];
         if(i == argc - 2)
